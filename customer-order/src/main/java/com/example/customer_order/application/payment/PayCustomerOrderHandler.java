fdead997c878b739971f6fdad052c637b2341838
lukas1299 <lukaszmatusik14@gmail.com>
2026-06-17 09:04:48 +0200

    Delete order

--- customer-order/src/main/java/com/example/customer_order/application/payment/PayCustomerOrderHandler.java
+++ customer-order/src/main/java/com/example/customer_order/application/payment/PayCustomerOrderHandler.java
@@ -3,6 +3,7 @@ package com.example.customer_order.application.payment;
 import com.example.customer_order.adapter.output.outbox.SpringCustomerOrderEventPublisher;
 import com.example.customer_order.domain.model.order.CustomerOrder;
 import com.example.customer_order.adapter.output.persistence.payment.PayCustomerOrderRepositoryPort;
+import com.example.customer_order.domain.model.order.exception.CustomerOrderNotFoundException;
 import org.springframework.transaction.annotation.Transactional;
 
 public class PayCustomerOrderHandler {
@@ -18,7 +19,7 @@ public class PayCustomerOrderHandler {
     @Transactional
     public void handle(PayCustomerOrderCommand command) {
         CustomerOrder order = repositoryPort.findById(command.orderId())
-                .orElseThrow(() -> new IllegalArgumentException("Nie znaleziono zamówienia: " + command.orderId()));
+                .orElseThrow(() -> new CustomerOrderNotFoundException(command.orderId()));
 
         order.pay();
 
